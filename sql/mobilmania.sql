1c6e8ac299247f610f48dcd247b253161563b792
Kovács László <lacikovacs330@gmail.com>
2023-08-01 13:59:43 +0200

    FULL 100%

--- sql/mobilmania.sql
+++ sql/mobilmania.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Júl 31. 11:25
+-- Létrehozás ideje: 2023. Aug 01. 13:58
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -38,7 +38,22 @@ CREATE TABLE `colors` (
 --
 
 INSERT INTO `colors` (`id_color`, `id_phone`, `color`) VALUES
-(149, 69, 'blue');
+(150, 73, 'blue'),
+(151, 73, 'black');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `contact`
+--
+
+CREATE TABLE `contact` (
+  `id_contact` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -81,9 +96,11 @@ INSERT INTO `manufacturers` (`id_manufacturer`, `manufacturer`) VALUES
 CREATE TABLE `orders` (
   `id_order` int(11) NOT NULL,
   `id_phone` int(11) NOT NULL,
+  `order_number` varchar(255) NOT NULL,
   `color` varchar(255) NOT NULL,
   `quantity` int(11) NOT NULL,
   `price` int(11) NOT NULL,
+  `storage` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
@@ -98,8 +115,8 @@ CREATE TABLE `orders` (
 -- A tábla adatainak kiíratása `orders`
 --
 
-INSERT INTO `orders` (`id_order`, `id_phone`, `color`, `quantity`, `price`, `id_user`, `firstname`, `lastname`, `phonenumber`, `delivery_method`, `city`, `postOffice`, `date`) VALUES
-(37, 69, 'blue', 3, 4500, 73, 'Kovács', 'Szilvia', '+36202826172', 'Home delivery', 'Mosonmagyarovar', '', '2023-07-28');
+INSERT INTO `orders` (`id_order`, `id_phone`, `order_number`, `color`, `quantity`, `price`, `storage`, `id_user`, `firstname`, `lastname`, `phonenumber`, `delivery_method`, `city`, `postOffice`, `date`) VALUES
+(85, 73, '20230801-e0348737', 'black', 3, 4500, 128, 73, 'Kovács', 'Szilvia', '+36202826172', 'Home delivery', 'Mosonmagyarovar', '', '2023-08-01');
 
 -- --------------------------------------------------------
 
@@ -158,7 +175,26 @@ CREATE TABLE `phones` (
 --
 
 INSERT INTO `phones` (`id_phone`, `id_manufacturer`, `model`, `price`, `operating_system`, `processor`, `operating_system_v`, `sim`, `screen_size`, `capacity`, `fm_radio`, `ram`, `external`, `internal`, `main_primary_camera`, `main_flash`, `main_video_record`, `main_face_detect`, `main_autofocus`, `main_led_flash`, `secondary_second`, `second_smile_detection`, `second_video`, `second_led_flash`, `second_flash`, `second_autofocus`, `wifi`, `bluetooth`, `usb`, `nfc`, `gps`, `mobile_network`, `2g`, `3g`, `4g`, `5g`, `weight`, `sms`, `email`, `height`, `width`, `length`, `visible`) VALUES
-(69, 6, 'Iphone 11', 1500, 'iOS 15', 'Apple A15 Bionic, 6-core Processor 3.2GHz and 4-core Graphics', 15, 'Dual SIM', 6.1, 3240, 'yes', 4, 'yes', 128, '12', 'yes', 'yes', 'yes', 'yes', 'yes', '12', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'A-GPS', 'yes', 'yes', 'yes', 'yes', 'yes', 174, 'yes', 'yes', 146.7, 7.65, 71.505, 1);
+(73, 6, 'Iphone 11', 1500, 'iOS 15', 'Apple A15 Bionic, 6-core Processor 3.2GHz and 4-core Graphics', 15, 'Dual SIM', 6.1, 3240, 'yes', 4, 'yes', 128, '12', 'yes', 'yes', 'yes', 'yes', 'yes', '12', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'A-GPS', 'yes', 'yes', 'yes', 'yes', 'yes', 174, 'yes', 'yes', 146.7, 7.65, 71.505, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `quantity`
+--
+
+CREATE TABLE `quantity` (
+  `id_quantity` int(11) NOT NULL,
+  `id_phone` int(11) NOT NULL,
+  `number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `quantity`
+--
+
+INSERT INTO `quantity` (`id_quantity`, `id_phone`, `number`) VALUES
+(2, 73, 460);
 
 -- --------------------------------------------------------
 
@@ -190,7 +226,7 @@ CREATE TABLE `storage` (
 --
 
 INSERT INTO `storage` (`id_storage`, `id_phone`, `storage`) VALUES
-(13, 69, 128);
+(15, 73, 128);
 
 -- --------------------------------------------------------
 
@@ -214,7 +250,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_user`, `username`, `password`, `email`, `token`, `status`, `role`) VALUES
 (70, 'lacikovacs330', '$2y$10$8PJEXpCiJcqvsEacjDvTr.TO9d0tnNKlIuwehMDRKg1kEGxxdpg72', 'lacikovacs330@gmail.com', '82b5583c24d248ddd212fb7ddd11a365', 1, 'admin'),
-(73, 'lacikovacs333', '$2y$10$MlwBSsAl6CFptPeCrJnFTez0oivej3J3WnxwqbsIzbRB7hGQL0bte', 'lacikovacs333@gmail.com', '39576e09109a2f676e81d8c51db1017d', 1, 'user');
+(73, 'lacikovacs333', '$2y$10$rSPAYsvPtcwN93jxKKDRKuyV5hfEEKmZnGw05fWcN5WrDkeQF2SSi', 'lacikovacs333@gmail.com', '39576e09109a2f676e81d8c51db1017d', 1, 'user');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -226,6 +262,13 @@ INSERT INTO `users` (`id_user`, `username`, `password`, `email`, `token`, `statu
 ALTER TABLE `colors`
   ADD PRIMARY KEY (`id_color`),
   ADD KEY `id_phone` (`id_phone`);
+
+--
+-- A tábla indexei `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id_contact`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- A tábla indexei `favourites`
@@ -257,6 +300,13 @@ ALTER TABLE `phones`
   ADD KEY `id_manufacturer` (`id_manufacturer`);
 
 --
+-- A tábla indexei `quantity`
+--
+ALTER TABLE `quantity`
+  ADD PRIMARY KEY (`id_quantity`),
+  ADD KEY `id_phone` (`id_phone`);
+
+--
 -- A tábla indexei `ratings`
 --
 ALTER TABLE `ratings`
@@ -285,7 +335,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `colors`
 --
 ALTER TABLE `colors`
-  MODIFY `id_color` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=150;
+  MODIFY `id_color` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=152;
+
+--
+-- AUTO_INCREMENT a táblához `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id_contact` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT a táblához `favourites`
@@ -297,19 +353,25 @@ ALTER TABLE `favourites`
 -- AUTO_INCREMENT a táblához `manufacturers`
 --
 ALTER TABLE `manufacturers`
-  MODIFY `id_manufacturer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_manufacturer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT a táblához `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 
 --
 -- AUTO_INCREMENT a táblához `phones`
 --
 ALTER TABLE `phones`
-  MODIFY `id_phone` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id_phone` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+
+--
+-- AUTO_INCREMENT a táblához `quantity`
+--
+ALTER TABLE `quantity`
+  MODIFY `id_quantity` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT a táblához `ratings`
@@ -321,7 +383,7 @@ ALTER TABLE `ratings`
 -- AUTO_INCREMENT a táblához `storage`
 --
 ALTER TABLE `storage`
-  MODIFY `id_storage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_storage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT a táblához `users`
@@ -358,6 +420,12 @@ ALTER TABLE `orders`
 --
 ALTER TABLE `phones`
   ADD CONSTRAINT `phones_ibfk_1` FOREIGN KEY (`id_manufacturer`) REFERENCES `manufacturers` (`id_manufacturer`);
+
+--
+-- Megkötések a táblához `quantity`
+--
+ALTER TABLE `quantity`
+  ADD CONSTRAINT `quantity_ibfk_1` FOREIGN KEY (`id_phone`) REFERENCES `phones` (`id_phone`);
 
 --
 -- Megkötések a táblához `ratings`
